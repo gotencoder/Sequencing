7ef6fa15bc7d9b18a02441390e72e892aff89bd6
imi <i.nasim@surrey.ac.uk>
2021-12-19 09:13:47 +0000

    updated

--- sample_list_titan_yaml.py
+++ sample_list_titan_yaml.py
@@ -9,28 +9,25 @@ import yaml
 
 
 
-#Loop over the indexes in the excel file
-excel_file = 'TCGA_Xena_15_TPM_cutoff_IDs_for_Imran.xlsx'
+'''#Load in our excel file
+excel_file = 'LUSC_and_TGCT_revised.xlsx'
 
-workbook = openpyxl.load_workbook(excel_file)
-
-sheet = workbook.active
-
-index_list = []
-concat=True
-for row in sheet.iter_rows(min_row=3,max_col=1):
-        for cell in row:
-            if (cell.value!=None):
-                #print(cell.value, end="")
-                if (concat==True):
-                    index_list += [cell.value[:-3]]
-                else:
-                    index_list += [cell.value]
-
-        #print()
+df = pd.read_excel(excel_file,engine='openpyxl')
+tumour_sample_column = np.array( [elem[:-3] for i, elem in enumerate(list(df['Truncated_Barcodes']))] )'''
 
 
+#excel_file = '6_4_TPM_geq3_XIST_pos_cutoff_IDs_for_Imran.xlsx'
+#excel_file = 'XIST_neg_NSGCTs.xlsx'
+#excel_file = 'samples_to_rerun_new_extension.xlsx'
+excel_file = 'TCGA_tRCCs_purity_ploidy.xlsx'
+df = pd.read_excel(excel_file,engine='openpyxl')
+#csv_file = 'random_172_XISTpos_females.csv'
+#csv_file = 'random_86_XISTneg_males.csv'
+#df = pd.read_csv(csv_file)
+tumour_sample_column = np.array( [elem[:-3] for i, elem in enumerate(list(df['Barcode']))] )
+#tumour_sample_column = np.array( [elem[:-3] for i, elem in enumerate(list(df['Tumor_Sample_Barcode']))] )
 
+#tumour_sample_column = np.array(['TCGA-77-6843'])
 
 filename = 'tcga_WXS_BAM_HG38_FILES_gdc_sample_sheet.tsv'
 
@@ -38,33 +35,40 @@ i=0
 df = pd.read_csv(filename, sep='\t')
 col_list = list(df.columns)  #List column names
 #print(col_list)
-sample_type_tum = ['Primary Tumor', 'Metastatic']# 
+sample_type_tum = ['Primary Tumor', 'Metastatic','Primary Blood Derived Cancer - Peripheral Blood']# 
+#sample_type_tum = ['Recurrent Tumor','Additional - New Primary']
 sample_type_normal = ['Blood Derived Normal', 'Solid Tissue Normal'] #Two types of normals we can have
 
 #Now check that the ID are present
 
 ref_list = df['Case ID']
 
-list1_as_set = set(index_list)
+'''list1_as_set = set(index_list)
 intersection = list1_as_set.intersection(ref_list)
 
 case_id = [] #Reset the case id as we need the IDs that are present in the table
 
 for z in intersection:
-        case_id += [z]
+        case_id += [z]'''
 
-
+#Get unique TCGA ids so we can obtain their path
+case_id = np.unique(tumour_sample_column)
+n_data = len(case_id)
 
 
 sample_list_for_titan1 = []
 sample_list_for_titan2 = []
 total_n = len(case_id)
-for i in range(1):
+wanted_index = []
+for i in range(n_data):
     #Get the index that matches the case_id
     index_tum = np.where((df['Case ID']==case_id[i]) & ( (df['Sample Type']==sample_type_tum[0]) | (df['Sample Type']==sample_type_tum[1]) ) )[0] #Get the index for the tumour
 
     index_norm = np.where((df['Case ID']==case_id[i]) & ( (df['Sample Type'] == sample_type_normal[0]) | (df['Sample Type'] == sample_type_normal[1]) ))[0] #Get the index for the tumour
-
+ 
+    if (len(index_tum)==0 or len(index_norm)==0):
+        print (i)
+        continue
 
     #We now want both the file_id and filename
     file_id_tum = np.array(df['File ID'][index_tum])[0]
@@ -103,18 +107,19 @@ for i in range(1):
                 normal_file=filename
     
     #We now want a script to output the filenames and directories
-    sample_list_for_titan1 += ['tumour_sample_'+str(i+1)]# +': '+tum_path+tum_file]    #+1 to start from 1        
-    sample_list_for_titan1 += ['normal_sample_'+str(i+1)]#+': '+norm_path+normal_file]    
+    sample_list_for_titan1 += [case_id[i]+'_tumour_sample_'+str(i+1)]# +': '+tum_path+tum_file]    #+1 to start from 1        
+    sample_list_for_titan1 += [case_id[i]+'_normal_sample_'+str(i+1)]#+': '+norm_path+normal_file]    
     sample_list_for_titan2 += [tum_path+tum_file]
     sample_list_for_titan2 += [norm_path+normal_file]
+    wanted_index += [i]
 
 
 i=0
 pairings_text1 = []
 pairings_text2 = []
-for i in range(1):
-    pairings_text1 += ['tumour_sample_'+str(i+1)]#+': '+'normal_sample_'+str(i+1)]
-    pairings_text2 += ['normal_sample_'+str(i+1)]
+for i in wanted_index:
+    pairings_text1 += [case_id[i]+'_tumour_sample_'+str(i+1)]#+': '+'normal_sample_'+str(i+1)]
+    pairings_text2 += [case_id[i]+'_normal_sample_'+str(i+1)]
 
 
 #Now make nested dictionaries
@@ -139,16 +144,17 @@ for i in range(len(pairings_text1)):
 #We need to save as a yaml file
 
 #dict_file = [{'samples' : res1},{'pairings' : res2 }]
-
-#output_file = 'output.yaml'
-#with open(output_file, 'w') as file:
-#    documents = yaml.dump(dict1, file)
+'''
+output_file = 'samples_trcc.yaml'
+with open(output_file, 'w') as file:
+    documents = yaml.dump(dict1, file)
 
 print(yaml.dump(dict1, sort_keys=False))
+'''
+#Print out the tumour/normal sample reference with their corresponding sample id 
 
 
-
-
+    
 
 
 
