e0b08c6c24ff1faa168fd715aabad54c087cbdcd
imi <i.nasim@surrey.ac.uk>
2022-01-06 11:28:13 +0000

    Minore update to load in required modules for running snakemake

--- wrapper.sh
+++ wrapper.sh
@@ -4,6 +4,10 @@
 #initialize conda
 #activate conda env
 
+module load bcftools/1.10.2
+module load samtools/1.10
+module load R/4.04 
+
 python3 TCGA_DS_script.py
 valslist='LIFTOVER_whole_exome_agilent_1.1_refseq_plus_3_boosters.targetIntervals.with_chr.hg38.bed LIFTOVER_hg18_nimblegen_exome_version_2.hg38.bed LIFTOVER_VCRome_2_1_hg19_primary_targets.hg38.bed LIFTOVER_SeqCap_EZ_Exome_v2.hg38.bed LIFTOVER_SeqCap_EZ_Exome_v3_hg19_capture_targets.hg38.bed LIFTOVER_SureSelect_Human_All_Exon_V2_Regions.hg38.bed'
 
